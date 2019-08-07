f94f68694ae57a8561c6aabcda644f90429bf25f
Zhangfei Gao <zhangfei.gao@linaro.org>
2019-08-07 15:23:58 +0800

    uacce: sync uacce.h
    Signed-off-by: Zhangfei Gao <zhangfei.gao@linaro.org>

--- include/uacce.h
+++ include/uacce.h
@@ -9,32 +9,23 @@
 #define UACCE_DEV_ATTRS		"attrs"
 #define UACCE_CMD_SHARE_SVAS	_IO('W', 0)
 #define UACCE_CMD_START		_IO('W', 1)
-#define UACCE_CMD_GET_SS_PA	_IOR('W', 2, unsigned long)
+#define UACCE_CMD_GET_SS_DMA	_IOR('W', 2, unsigned long)
 
 /**
  * UACCE Device Attributes:
  *
  * NOIOMMU: the device has no IOMMU support
- *	can do ssva, but no map to the dev
+ *	can do share sva, but no map to the dev
  * PASID: the device has IOMMU which support PASID setting
- *	can do ssva, mapped to dev per process
+ *	can do share sva, mapped to dev per process
  * FAULT_FROM_DEV: the device has IOMMU which can do page fault request
- *	no need for ssva, should be used with PASID
- * KMAP_DUS: map the Device user-shared space to kernel
- * DRVMAP_DUS: mmap dus with driver's callback
- * 	used by NOIOMMU for handling its own mapping
- * CONT_PAGE: allocate continue physical page for region
- * 	used by NOIOMMU if necessary
+ *	no need for share sva, should be used with PASID
  * SVA: full function device
- * SHARE_DOMAIN: no PASID, can do ssva only for one process and the kernel
+ * SHARE_DOMAIN: no PASID, can do share sva only for one process and the kernel
  */
-#define UACCE_DEV_NOIOMMU		(1<<0)
-#define UACCE_DEV_PASID			(1<<1)
-#define UACCE_DEV_FAULT_FROM_DEV	(1<<2)
-#define UACCE_DEV_KMAP_DUS		(1<<3)
-#define UACCE_DEV_DRVMAP_DUS		(1<<4)
-#define UACCE_DEV_CONT_PAGE		(1<<5)
-
+#define UACCE_DEV_NOIOMMU		(1 << 0)
+#define UACCE_DEV_PASID			(1 << 1)
+#define UACCE_DEV_FAULT_FROM_DEV	(1 << 2)
 #define UACCE_DEV_SVA		(UACCE_DEV_PASID | UACCE_DEV_FAULT_FROM_DEV)
 #define UACCE_DEV_SHARE_DOMAIN	(0)
 
