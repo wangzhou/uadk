89ed433d33995eb6fa3d5ab89618dcd1465085a4
Kai Ye <yekai13@huawei.com>
2020-06-12 13:58:26 +0800

    sec: add hisi sec sqe description.
    Signed-off-by: Kai Ye <yekai13@huawei.com>

--- include/sec_usr_if.h
+++ include/sec_usr_if.h
@@ -5,9 +5,19 @@
 #include <asm/types.h>
 
 
-
-
-
-
+struct hisi_sec_sqe {
+	__u32 type;
+	__u32 cipher;
+	__u32 auth;
+	__u32 seq;
+	__u32 de;
+	__u32 scene;
+	__u32 src_addr_type;
+	__u32 dst_addr_type;
+	__u32 mac_addr_type;
+	__u32 rsvd0;
+	
+	struct hisi_sec_sqe_type2 type2;
+};
 
 #endif
