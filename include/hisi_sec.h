f5b5758c5553df52ed73bdcb7210327dcd9c3a51
Kai Ye <yekai13@huawei.com>
2020-06-15 11:13:21 +0800

    sec: add digest alg and mode.
    Signed-off-by: Kai Ye <yekai13@huawei.com>

--- include/hisi_sec.h
+++ include/hisi_sec.h
@@ -15,6 +15,41 @@ enum C_ALG {
 	C_ALG_SM4  = 0x3,
 };
 
+enum A_ALG {
+	A_ALG_SHA1	 = 0x0,
+	A_ALG_SHA256 = 0x1,
+	A_ALG_MD5	 = 0x2,
+	A_ALG_SHA224 = 0x3,
+	A_ALG_SHA384 = 0x4,
+	A_ALG_SHA512 = 0x5,
+	A_ALG_SHA512_224 = 0x6,
+	A_ALG_SHA512_256 = 0x7,
+	A_ALG_HMAC_SHA1   = 0x10,
+	A_ALG_HMAC_SHA256 = 0x11,
+	A_ALG_HMAC_MD5	  = 0x12,
+	A_ALG_HMAC_SHA224 = 0x13,
+	A_ALG_HMAC_SHA384 = 0x14,
+	A_ALG_HMAC_SHA512 = 0x15,
+	A_ALG_HMAC_SHA512_224 = 0x16,
+	A_ALG_HMAC_SHA512_256 = 0x17,
+	A_ALG_AES_XCBC_MAC_96  = 0x20,
+	A_ALG_AES_XCBC_PRF_128 = 0x20,
+	A_ALG_AES_CMAC = 0x21,
+	A_ALG_AES_GMAC = 0x22,
+	A_ALG_SM3	   = 0x25,
+	A_ALG_HMAC_SM3 = 0x26
+};
+
+enum C_MODE {
+	C_MODE_ECB	  = 0x0,
+	C_MODE_CBC	  = 0x1,
+	C_MODE_CTR	  = 0x4,
+	C_MODE_CCM	  = 0x5,
+	C_MODE_GCM	  = 0x6,
+	C_MODE_XTS	  = 0x7,
+	C_MODE_CBC_CS     = 0x9,
+};
+
 extern int hisi_cipher_init(struct wd_cipher_sess *sess);
 extern void hisi_cipher_exit(struct wd_cipher_sess *sess);
 extern int hisi_cipher_prep(struct wd_cipher_sess *sess, struct wd_cipher_arg *arg);
