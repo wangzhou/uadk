a4314eb564ac69055d88f71c38871a7a87eb4e40
Kai Ye <yekai13@huawei.com>
2020-06-22 11:26:15 +0800

    test: add test hisi sec code.
    Signed-off-by: Kai Ye <yekai13@huawei.com>

---
+++ test/test_hisi_sec.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+
+#define HW_CTX_SIZE (24 * 1024)
+
+int main(int argc, char *argv[])
+{
+	printf("this is a hisi sec test.\n");
+	return 0;
+}
---
+++ test/test_hisi_sec.h
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0+
+#ifndef TEST_HISI_SEC_H_
+#define TEST_HISI_SEC_H_
+#endif /* TEST_HISI_SEC_H_ */
