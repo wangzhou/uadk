2275d4a0ffc6bdd15067c670b31d89b56c19329d
Kai Ye <yekai13@huawei.com>
2020-06-23 10:26:50 +0800

    test: add test fix config.
    Signed-off-by: Kai Ye <yekai13@huawei.com>

--- test/test_hisi_sec.h
+++ test/test_hisi_sec.h
@@ -1,4 +1,34 @@
 // SPDX-License-Identifier: GPL-2.0+
 #ifndef TEST_HISI_SEC_H_
-#define TEST_HISI_SEC_H_
+#define TEST_HISI_SEC_H
+enum cipher_op_type {
+	ENCRYPTION,
+	DECRYPTION,
+};
+enum cipher_alg {
+	CIPHER_SM4,
+	CIPHER_AES,
+	CIPHER_DES,
+	CIPHER_3DES,
+};
+
+enum cipher_mode {
+	ECB,
+	CBC,
+	CTR,
+	XTS,
+};
+
+struct cipher_testvec {
+	const char *key;
+	int klen;
+	const chat *iv;
+	int ivlen;
+	const char *iv_out;
+	const char *ptext;
+	const char *ctext;
+	int len;
+}
+
+
 #endif /* TEST_HISI_SEC_H_ */
